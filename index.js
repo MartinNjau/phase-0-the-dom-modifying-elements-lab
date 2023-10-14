5341cdcb59ee6bb2e3e331dddd9e758723508a0e
Martin Njau <njau_martin@yahoo.comgit config --global user.email njau_martin@yahoo.comclearclear>
2023-10-14 21:28:05 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// Write your code here!
+document.getElementById('main');
+main.remove();
+
+const newHeader=document.createElement('h1');
+
+    newHeader.id = 'victory';
+    newHeader.textContent = ('Martin is the champion');
+
+document.body.appendChild(newHeader);
